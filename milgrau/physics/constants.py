a29454acc9d3cd8648cbdbd39067d6734af9e4d7
SPULiDAR Leal <inciteleal@gmail.com>
2026-05-11 19:09:27 -0300

    Add physics constants

---
+++ milgrau/physics/constants.py
@@ -0,0 +1,9 @@
+"""Physical constants used by MILGRAU."""
+
+from __future__ import annotations
+
+import numpy as np
+
+SPEED_OF_LIGHT_M_S = 299_792_458.0
+BOLTZMANN_CONSTANT_J_K = 1.380649e-23
+RAYLEIGH_LIDAR_RATIO_SR = 8.0 * np.pi / 3.0
