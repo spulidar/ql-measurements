8f928eec5a3469a929993f2a65368a3021f57ab2
SPULiDAR Leal <inciteleal@gmail.com>
2026-05-11 19:18:39 -0300

    Create visualization package exports

---
+++ milgrau/visualization/__init__.py
@@ -0,0 +1,6 @@
+"""Visualization helpers for MILGRAU products."""
+
+from milgrau.visualization.quicklooks import plot_global_mean_rcs, plot_quicklook
+from milgrau.visualization.level2_qa import plot_all_level2_qa
+
+__all__ = ["plot_all_level2_qa", "plot_global_mean_rcs", "plot_quicklook"]
