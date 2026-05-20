b9ec2534a65f0a77a0ee4aff429e8b584653ae79
SPULiDAR Leal <inciteleal@gmail.com>
2026-05-19 22:43:16 -0300

    Export path and contract helpers

--- milgrau/io/__init__.py
+++ milgrau/io/__init__.py
@@ -1,9 +1,11 @@
 """Input/output helpers for MILGRAU."""
 
+from milgrau.io.contracts import validate_level0_contract, validate_level1_contract, validate_level2_contract
 from milgrau.io.filesystem import ensure_directories, scan_raw_files
 from milgrau.io.inventory import build_measurement_inventory
 from milgrau.io.licel import parse_licel_group, read_licel_header
 from milgrau.io.logging_utils import setup_logger
+from milgrau.io.paths import level0_output_path, level1_output_path, level2_output_path, measurement_save_id
 from milgrau.io.radiosonde import fetch_wyoming_radiosonde
 from milgrau.io.weather import fetch_surface_weather
 
@@ -12,8 +14,15 @@ __all__ = [
     "ensure_directories",
     "fetch_surface_weather",
     "fetch_wyoming_radiosonde",
+    "level0_output_path",
+    "level1_output_path",
+    "level2_output_path",
+    "measurement_save_id",
     "parse_licel_group",
     "read_licel_header",
     "scan_raw_files",
     "setup_logger",
+    "validate_level0_contract",
+    "validate_level1_contract",
+    "validate_level2_contract",
 ]
